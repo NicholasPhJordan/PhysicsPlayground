22bcb521ce03e05fc06bae2e913c3b7b6c8b29b1
NicholasPhJordan <NicholasPhJordan@gmail.com>
2021-08-27 16:02:23 -0500

    Working on VehicleBehaviour script to make car move

---
+++ Assets/Scripts/VehicleBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleBehaviour : MonoBehaviour
+{
+    public HingeJoint backLeft;
+    public HingeJoint backRight;
+
+    private JointMotor rearLeft;
+    private JointMotor rearRight;
+
+    private void Start()
+    {
+        rearLeft = backLeft.motor;
+        rearRight = backRight.motor;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rearLeft.targetVelocity = -30000;
+            rearRight.targetVelocity = 30000;
+        }
+        else
+        {
+            rearLeft.targetVelocity = 0;
+        }
+    }
+}
