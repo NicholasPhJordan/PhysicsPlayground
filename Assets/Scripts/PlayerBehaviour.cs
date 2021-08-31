1d3b40801d0131cef30ad069c6f945b1578d9454
NicholasPhJordan <NicholasPhJordan@gmail.com>
2021-08-31 16:28:22 -0500

    Got player movement based off camera / Started CameraController

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpStrength;
-    [SerializeField] private float gravityModifier;
+    [SerializeField] public float _speed;
+    [SerializeField] public float _jumpStrength;
+    [SerializeField] public float _airControl = 1.0f; 
+    [SerializeField] public float _gravityModifier;
+
+    [SerializeField] public Camera _playerCamera;
 
     private CharacterController _controller;
 
     private Vector3 _desiredVelcoity;
     private Vector3 _airvelocity;
-    private bool _jumpIsDesired;
+    private bool _jumpIsDesired = false;
+    private bool _isGrounded = false;
 
     private void Awake()
     {
@@ -26,26 +30,46 @@ public class PlayerBehaviour : MonoBehaviour
         _desiredVelcoity.y = 0.0f;
         _desiredVelcoity.z = Input.GetAxis("Vertical");
 
+        //Get Camera forward
+        Vector3 cameraForward = _playerCamera.transform.forward;
+        cameraForward.y = 0.0f;
+        cameraForward.Normalize();
+
+        //Get camera right
+        Vector3 cameraRight = _playerCamera.transform.right;
+        _desiredVelcoity = _desiredVelcoity.x * cameraRight + _desiredVelcoity.z * cameraForward;
+
         //Get jump input
-        _jumpIsDesired = Input.GetButtonDown("Jump");
+        _jumpIsDesired = Input.GetButton("Jump");
 
         //Set movement magnitude (and speed)
         _desiredVelcoity.Normalize();
-        _desiredVelcoity *= speed;
+        _desiredVelcoity *= _speed;
+
+        //Apply air control
+
+
+        //Check for ground
+        _isGrounded = _controller.isGrounded;
 
         //Apply jump strength 
-        if (_jumpIsDesired)
+        if (_jumpIsDesired && _isGrounded)
         {
-            _airvelocity.y = jumpStrength;
+            _airvelocity.y = _jumpStrength;
             _jumpIsDesired = false;
         }
 
-        //Apply Gravity
-        _airvelocity += Physics.gravity * gravityModifier * Time.deltaTime;
+        //Stop on ground
+        if (_isGrounded && _airvelocity.y < 0.0f)
+            _airvelocity.y = -1.0f;
 
+        //Apply Gravity
+        _airvelocity += Physics.gravity * _gravityModifier * Time.deltaTime;
+
+        //Add air velocity
         _desiredVelcoity += _airvelocity;
 
-        //Apply Move
+        //Move
         _controller.Move((_desiredVelcoity + _airvelocity) * Time.deltaTime);
     }
 }
