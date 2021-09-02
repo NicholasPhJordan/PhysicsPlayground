fef30e04942b03adde194e40500d803162cb80c2
NicholasPhJordan <NicholasPhJordan@gmail.com>
2021-09-02 10:34:00 -0500

    Added collision checks for the camera
    -regenerated lighting 
    -messed with wall prefab
    -added a wall to the physics scene 
    -added a camera target object to the player prefab

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform _target;
-    [SerializeField] private float _distanceFromTarget = 0.0f;
-    [SerializeField] private float sensitivity = 0.0f;
-    [SerializeField] private bool invertY = false;
+    [SerializeField] private float _maxDistance = 0.0f;
+    [SerializeField] private float _sensitivity = 0.0f;
+    [SerializeField] private bool _invertY = false;
+    [SerializeField] private float returnSpeed = 0.0f;
+
+    private float _currentDistance;
+
+    private void Start()
+    {
+        _currentDistance = _maxDistance;
+    }
 
     private void Update()
     {
@@ -19,20 +27,31 @@ public class CameraController : MonoBehaviour
 
             //Get input
             Vector2 rotation;
-            rotation.x = Input.GetAxis("Mouse Y") * (invertY ? -1.0f : 1.0f);
+            rotation.x = Input.GetAxis("Mouse Y") * (_invertY ? -1.0f : 1.0f);
             rotation.y = Input.GetAxis("Mouse X");
 
             //Look up and down by rotating around the X-axis
-            angles.x = Mathf.Clamp(angles.x + rotation.x * sensitivity, 0.0f, 70.0f);
+            angles.x += rotation.x * _sensitivity;
+            angles.x = Mathf.Clamp(angles.x, 0.0f, 70.0f);
 
             //Look left and right by rotating around the Y-axis
-            angles.y += rotation.y * sensitivity;
+            angles.y += rotation.y * _sensitivity;
 
             //Set the angles
             transform.eulerAngles = angles;
         }
 
         //Move the camera
-        transform.position = _target.position + (_distanceFromTarget * -transform.forward);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(_target.position, -transform.forward, out hitInfo, _maxDistance))
+        {
+            _currentDistance = hitInfo.distance;
+        }
+        else
+        {
+            _currentDistance = Mathf.MoveTowards(_currentDistance, _maxDistance, returnSpeed * Time.deltaTime);
+        }
+
+        transform.position = _target.position + (_currentDistance * -transform.forward);
     }
 }
