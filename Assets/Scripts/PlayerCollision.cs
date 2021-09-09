3effb2f5ff791fb2bd2132963c06f3dafef425ac
NicholasPhJordan <NicholasPhJordan@gmail.com>
2021-09-09 11:12:01 -0500

    Added Ragdoll to UnityChan & added her to physics scene
    -made minor changes to the unity scene

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.CompareTag("Killer"))
+        {
+            _animator.enabled = false;
+            Debug.Log("you died bitch");
+        }
+    }
+}
