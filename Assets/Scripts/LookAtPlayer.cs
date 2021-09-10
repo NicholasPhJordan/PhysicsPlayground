2025eba869ab0064d0193464ab78d6932bf4993c
NicholasPhJordan <NicholasPhJordan@gmail.com>
2021-09-10 16:01:40 -0500

    Boss can look at enemy

---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : ProjectileLauncher
+{
+    [SerializeField] private GameObject _lookAtTarget;
+    [SerializeField] private float _waitTime;
+
+    void Update()
+    {
+        if (_lookAtTarget)
+        {
+            transform.LookAt(_lookAtTarget.transform);
+        }
+    }
+
+    IEnumerator ShootPlayer(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        LaunchProjectile();
+    }
+}
