6f1f2d71ecf7adaeb7c74c6050b5048073eb3393
NicholasPhJordan <NicholasPhJordan@gmail.com>
2021-09-17 15:38:02 -0500

    Projectiles get removed & player respawns on last platform

---
+++ Assets/ProjectileBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float destroyHieght = 20.0f;
+
+    private void Update()
+    {
+        if (projectile.transform.position.y <= destroyHieght)
+        {
+            Destroy(projectile);
+        }
+    }
+}
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -5,24 +5,22 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
-    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject _player;
 
-    private bool playerScript;
-    private Vector3 homePos;
+    private bool _playerScript;
+    private Vector3 _homePos;
 
     private void Start()
     {
-        homePos = player.transform.position;
-
-        playerScript = player.GetComponent<PlayerBehaviour>().enabled;
+        _playerScript = _player.GetComponent<PlayerBehaviour>().enabled;
     }
 
     private void Update()
     {
-        if (player.transform.position.y <= 20.0f)
+        if (_player.transform.position.y <= 20.0f)
         {
             _animator.enabled = false;
-            playerScript = false;
+            _playerScript = false;
         }
     }
 
@@ -31,11 +29,16 @@ public class PlayerCollision : MonoBehaviour
         if (hit.gameObject.CompareTag("Killer"))
         {
             _animator.enabled = false;
-            playerScript = false;
+            _playerScript = false;
+        }
+        else if (hit.gameObject.CompareTag("Platform"))
+        {
+            _homePos = _player.transform.position;
+            _homePos.y += 1;
         }
         else if (hit.gameObject.CompareTag("Void"))
         {
-            player.transform.position = homePos;
+            _player.transform.position = _homePos;
             _animator.enabled = true;
         }
     }
